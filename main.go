fdaca4b7d85733d405e6bf0f274352554c1cd845
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-04-26 20:26:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -119,6 +119,6 @@ func main() {
 	}).Methods("DELETE")
 
 	// Start server
-	log.Println("Server listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Println("Server listening on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
